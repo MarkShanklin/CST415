ac8a91cbafe74b35a6a3d2ebd7f877bb14ec46e2
masisaries <masisaries@gmail.com>
2017-09-30 13:29:52 -0700

    cr

--- lab01/encode.c
+++ lab01/encode.c
@@ -28,9 +28,9 @@ void *encode(request_t *request, void *buff)
     {
         memcpy(request->service_name, ((request_t *)buff)->service_name, MAX_SERVICE_NAME_LEN); //(dest,src,length)
     }
-    ((request_t *)buff)->service_name[MAX_SERVICE_NAME_LEN] = '0'; //make last item in string NULL
+    ((request_t *)buff)->service_name[MAX_SERVICE_NAME_LEN] = '\0'; //make last item in string NULL
     int length = strlen(request->service_name);                    //find the length up to the first NULL
-    memset(buff + length, 0, MAX_SERVICE_NAME_LEN - length);       //NULL fill from first NULL to end
+    memset(buff + length, '\0', MAX_SERVICE_NAME_LEN - length);       //NULL fill from first NULL to end
     ((request_t *)buff)->msg_type = request->msg_type;             //copy msg type only one byte no need to network order
     ((request_t *)buff)->port = htons(request->port);              //copy port using network byte order
     ((request_t *)buff)->status = request->status;                 //copy status only one byte no need to network order
