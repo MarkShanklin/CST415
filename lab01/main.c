6a38958c7e1c752224f006aac022a226acf00d1b
masisaries <masisaries@gmail.com>
2017-09-30 13:45:45 -0700

    cr

--- lab01/main.c
+++ lab01/main.c
@@ -16,7 +16,32 @@ int main(void)
     request_t req;
     memset(req.service_name, 0, MAX_SERVICE_NAME_LEN + 1);
     char buff[MAX_SERVICE_NAME_LEN + 1];
+    memset(buff, 0, MAX_SERVICE_NAME_LEN + 1);
     char *buff_ptr;
+    for (int i = 0; i < 10; i++)
+    {
+        req.msg_type = i;
+        req.status = i;
+        strcpy(req.service_name, "this is the service name" + i);
+        req.port = 1000 + i;
+        buff_ptr = encode(&req, &buff);
+
+        printf("buff_ptr: %h\n",buff_ptr);
+        printf("req: %h\n", req);
+        printf("Buff: %s\n", buff);
+        printf("msg_type: %d\n", req.msg_type);
+        printf("status: %d\n", req.status);
+        printf("service_name: ");
+        for (int j = 0; j < MAX_SERVICE_NAME_LEN + 1; j++)
+        {
+            if(req.service_name[j] == '\0')
+            printf("0");
+            else
+            printf("%c",req.service_name[j]);
+        }
+        printf("\n");
+        printf("port: %d\n", req.port);
+    }
     for (int i = 0; i < 10; i++)
     {
         req.msg_type = i;
@@ -25,6 +50,8 @@ int main(void)
         req.port = 1000 + i;
         buff_ptr = encode(&req, &req);
 
+        printf("buff_ptr: %h\n",buff_ptr);
+        printf("req: %h\n", req);
         printf("Buff: %s\n", buff);
         printf("msg_type: %d\n", req.msg_type);
         printf("status: %d\n", req.status);
