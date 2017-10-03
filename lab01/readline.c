ff6da34051738c8ddcfffdc4180a3a5962fe567b
masisaries <masisaries@gmail.com>
2017-10-02 19:56:47 -0700

    cr

--- lab01/readline.c
+++ lab01/readline.c
@@ -7,9 +7,10 @@
 **********************************************************/
 #include "readblocks.h"
 
-typedef struct 
+typedef struct
 {
     int8_t placeHolder;
+    int8_t amount;
     char buffer[BLOCK_SIZE];
 } buffer_t;
 
@@ -17,15 +18,56 @@ static __thread buffer_t myBuff;
 
 char *readline(char *buff, ssize_t size, int fd)
 {
-    //int8_t amount;
-    if(myBuff.placeHolder == 0)
+    for (int j = 0;j < size;)
     {
-        myBuff.placeHolder = read_block(fd,myBuff.buffer); 
-    }
-    for(int i = myBuff.placeHolder; i < BLOCK_SIZE; i++)
-    {
-        
-        
+        if (myBuff.placeHolder == 0)
+        {
+            myBuff.amount = read_block(fd, myBuff.buffer);
+            if(j + myBuff.amount > size){
+                //overflow
+                memcpy(buff+j, &myBuff.buffer, (size-j))
+            } else {
+                j += myBuff.amount;
+            }
+            if (myBuff.amount == 0)
+            {
+                return buff;
+            }
+            for (int i = 0; i < myBuff.amount; i++)
+            {
+                if (myBuff.buffer[i] == '\n')
+                {
+                    myBuff.placeHolder = i;
+                    memcpy(buff+j, &myBuff.buffer, i + 1);
+                    myBuff.amount = myBuff.amount - myBuff.placeHolder;
+                    return buff;
+                }
+                else if (i == myBuff.amount - 1)
+                {
+                    myBuff.placeHolder = i;
+                    memcpy(buff+j, &myBuff.buffer, i + 1);
+                    myBuff.amount = myBuff.amount - myBuff.placeHolder;
+                }
+            }
+        }
+        else
+        {
+            for (int i = myBuff.placeHolder; i < myBuff.amount; i++)
+            {
+                if (myBuff.buffer[i] == '\n' || i == myBuff.amount - 1)
+                {
+                    memcpy(buff+j, &myBuff.buffer + myBuff.placeHolder, i + 1);
+                    myBuff.placeHolder = i;
+                    myBuff.amount = myBuff.amount - myBuff.placeHolder;
+                    return buff;
+                }
+                else if{
+                    memcpy(buff+j, &myBuff.buffer + myBuff.placeHolder, i + 1);
+                    myBuff.placeHolder = i;
+                    myBuff.amount = myBuff.amount - myBuff.placeHolder;
+                }
+            }
+        }
     }
     return buff;
 };
