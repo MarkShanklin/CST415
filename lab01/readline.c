230c336c37c61cfe08354fbcd300984618da2b00
masisaries <masisaries@gmail.com>
2017-10-03 16:03:00 -0700

    cr

--- lab01/readline.c
+++ lab01/readline.c
@@ -40,7 +40,7 @@ char *readline(char *buff, ssize_t size, int fd)
         } 
         else if (myBuff.amountRead <= 0)
         {
-            mybuff.amountCopied = 0;
+            myBuff.amountCopied = 0;
             myBuff.totalCopied = 0;
             return NULL;
         }
