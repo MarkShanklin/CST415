917a562bfa0c80b70a16f66d6b5ba2aceb968b35
masisaries <masisaries@gmail.com>
2017-10-03 10:02:32 -0700

    cr

--- lab01/readline.c
+++ lab01/readline.c
@@ -29,7 +29,7 @@ char *readline(char *buff, ssize_t size, int fd)
 
             if((myBuff.amount) > size){
                 memcpy(buff, myBuff.buffer, (size - 1));
-                myBuff.placeHolder += (size + 1);
+                myBuff.placeHolder += (size);
                 myBuff.amount -= (size - 1);
                 return buff;
             } 
