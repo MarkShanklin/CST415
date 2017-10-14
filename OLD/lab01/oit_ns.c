627400e22176e3212781073e1167509afd010c7c
Mark Shanklin <mark.shanklin@oit.edu>
2017-10-14 15:46:54 -0700

    changes

--- OLD/lab01/oit_ns.c
+++ OLD/lab01/oit_ns.c
@@ -2,7 +2,7 @@
 * File Name     : oit_ns.c
 * Purpose       : Service used to maintain unique port numbers.
 * Creation Date : 09-27-2016
-* Last Modified : Mon 10 Oct 2016 11:59:34 AM PDT
+* Last Modified : Sat 14 Oct 2017 03:44:26 PM PDT
 * Created By    : Mark Shanklin 
 ***********************************************************/
 #include <stdio.h>
@@ -13,19 +13,15 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <string.h>
-#include <bool.h>
 
 #include "nameserver.h"
 
 typedef struct list_element list_element;
-struct list_element 
+typedef struct list_element 
 {
     int keep_alive; //time stamp for keeping alive when i reqieve keep alive message update time stamp -1 equals dead.
     char service_name[MAX_SERVICE_NAME_LEN + 1];
     uint16_t port;
-
-    //list_element *next;
-    //list_element *previous;
 }service_t;
 
 int main(int argc, char *argv[])
@@ -35,7 +31,7 @@ int main(int argc, char *argv[])
     int keepAliveTime = 300; //seconds
     int command = 0;
     char buffer[1024];
-    bool verbose = false;
+    int verbose = 0;
  
     while((command = getopt(argc, argv, "p:n:t:hv")) != -1)
     {
@@ -57,7 +53,7 @@ int main(int argc, char *argv[])
                         "-t \t<keep alive time in seconds\n");
                 break;
             case 'v':
-                verbose = true;
+                verbose = 1;
             case '?':
                 break;
         }
@@ -75,18 +71,16 @@ int main(int argc, char *argv[])
     myaddr.sin_port = htons(servicePort);
 
     bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr));
-
-    printf("Bound: %d", nstoh(myaddr.port));
     
-    if(verbose)
+    if(verbose == 1)
     {
         printf("Service Port: %d\n"
                "Minimum Ports: %d\n"
                "Keep Alive Time: %d\n\n", servicePort, minimumPorts, keepAliveTime);
     }
     
-    while(true)
-    { recvfrom(fd, buffer, sizeof(buffer), 0, recv_addr, sizeof(recv_addr));
+    while(1)
+    { recvfrom(fd, buffer, sizeof(buffer), 0, &recv_addr, sizeof(recv_addr));
         //decode
         //process message, msg_type defines action if succeded success
         //encode msg_type=response status is depeinding
