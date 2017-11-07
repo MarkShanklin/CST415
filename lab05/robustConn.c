f870c65bfa92cda5c727a1ece095080a8c91efad
Mark Shanklin <mark.shanklin@oit.edu>
2017-11-07 15:55:01 -0800

    start lab5

---
+++ lab05/robustConn.c
@@ -0,0 +1,19 @@
+/***********************************************************
+* File Name     : robustConn.c
+* Purpose       : A more robust connection strategy.
+* Creation Date : 11-07-2017
+* Last Modified : Tue 07 Nov 2017 03:53:25 PM PST
+* Created By    : Mark Shanklin 
+***********************************************************/
+#include <stdio.h>
+
+
+
+/***********************************************************
+*
+***********************************************************/
+int main(int argc, int argv[]) {
+
+
+    return (EXIT_SUCESS);
+};
