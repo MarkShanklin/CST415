cd49eea9a02b82bb10a09975acc0c163ba95587a
masisaries <masisaries@gmail.com>
2017-11-07 18:42:42 -0800

    ch

--- lab05/robustConn.c
+++ lab05/robustConn.c
@@ -9,9 +9,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <netdb.h>
-#include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 #include <string.h>
 #include <getopt.h>
 #include <arpa/inet.h>
