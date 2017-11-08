42d28ed446d9361be22e23798eea5e82fa51e2e6
masisaries <masisaries@gmail.com>
2017-11-07 18:11:41 -0800

    finish

--- lab05/robustConn.c
+++ lab05/robustConn.c
@@ -5,15 +5,159 @@
 * Last Modified : Tue 07 Nov 2017 03:53:25 PM PST
 * Created By    : Mark Shanklin 
 ***********************************************************/
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <time.h>
+#include <getopt.h>
+#include <arpa/inet.h>
 
+/* Unfortunately some UNIX implementations define FALSE and TRUE -
+   here we'll undefine them */
 
+   #ifdef TRUE
+   #undef TRUE
+   #endif
+   
+   #ifdef FALSE
+   #undef FALSE
+   #endif
+   
+   typedef enum { FALSE, TRUE } Boolean;
 
 /***********************************************************
-*
+* a function to time out a read
 ***********************************************************/
-int main(int argc, int argv[]) {
+int timed_read(int sockfd, int timeout, void *buf, siz_t count)
+{
+    int result;
+    struct timeval tv;
+    fd_set rfds;
+    FD_ZERO(&rfds);
+    FD_SET(sockfd, &rfds);
+
+    tv.tv_sec = (long)timeout;
+    tv.tv_usec = 0;
+
+    result = select(sockfd+1, &rfds, (fd_set *) 0, (fd_set *) 0, &tv);
+    if(result > 0)
+    {
+        return read(sockfd, buf, count);
+    }
+    else
+    {
+        return result;
+    }
+}
+
+//*************************************************************
+int main(int argc, char *argv[])
+{
+    //50000. bound to port zero so that it will assign a port
+    int servicePort = 0;
+    //used to handle comand line argurements.
+    int command = 0; 
+    //used to signify if you want to print out data that
+    // is being process to the stdout.
+    int verbose = 0;
+
+    
 
 
-    return (EXIT_SUCESS);
+    //command line parsing.
+    while ((command = getopt(argc, argv, "hv")) != -1) 
+    {
+        switch (command)
+        {
+        case 'h':
+            printf( "This program is a service used to "
+                    "maintain unique port numbers.\n\n"
+                    "-p \t<service port>\n"
+                    "-n \t<minimum number of ports>\n"
+                    "-t \t<keep alive time in seconds\n");
+            break;
+        case 'v':
+            verbose = 1;
+        case '?':
+            break;
+        }
+    }
+
+    //setting up socket to a file discriptor.
+   // int fd = socket(AF_INET, SOCK_STREAM, 0); 
+    //
+    int server_socket;
+    struct addrinfo *servinfo, *p;
+    int ret_val;
+
+    char server_name[256];
+    char port[15] = "80";
+    const char *message = "GET / HTTP/1.1\r\nHost: bbc.com\r\nConnection:" 
+            "keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent:"
+            " Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML,"
+            " like Gecko) Chrome/61.0.3163.100 Safari/537.36\r\nAccept:"
+            " text/html,application/xhtml+xml,application/xml;q=0.9,image"
+            "/webp,image/apng,*/*;q=0.8\r\nAccept-Encoding: gzip, deflate"
+            "\r\nAccept-Language: en-US,en;q=0.8\r\n\r\n";
+    char responce[256];
+    //get host name from command line args
+    
+
+    if(ret_val = getaddrinfo(server_name,port,NULL,&servinfo) != 0)
+    {
+        fprintf(stderr,"getaddrinfo: %d\n", ret_val);
+        exit(1);
+    }
+    char dst[INET6_ADDRSTRLEN];
+    for (p=servinfo; p != NULL; p = p->ai_next)
+    {
+        switch(p->ai_socktype)
+        {
+            case 17:
+                fprintf(stdout,"UDP");
+                break;
+            case 6;
+                fprintf(stdout,"TCP");
+                break;
+            case 0:
+                fprintf(stdout,"RAW");
+                break;
+        }
+        fprintf(stdout," connection to %s",inet_ntop(p->ai_family, p->ai_addr,
+            dst, INET6_ADDRSTRLEN));
+        if((server_socket = 
+            socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+        {
+            fprintf(stderr,"Failed.");
+        }
+        else if(connect(server_socket, p->ai_addr, p->addrlen) != 0)
+        {
+            fprintf(stderr,"Failed.");
+        }
+        else
+        {
+            write(server_socket,message,sizeof(message));
+            if(timed_read(server_socket, 5, responce, 256) > 0)
+            {
+               fprintf(stdout, "Succeeded. Communicated.");
+            }
+            else{
+                fprintf(stderr, "Failed.");
+            }
+        }
+    }
+
+    //free address info
+    freeaddrinfo(&servinfo);
+
+    if(verbose == TRUE)
+    {
+        fprintf(stdout,"\nExiting with Success");
+    }
+    return (EXIT_SUCCESS);
 };
