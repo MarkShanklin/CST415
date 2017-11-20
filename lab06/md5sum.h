36f8cc9cdd5ad51bd9e35f717dc553b29b73c4e5
masisaries <masisaries@gmail.com>
2017-11-20 15:45:38 -0800

    changes

--- lab06/md5sum.h
+++
@@ -1,8 +0,0 @@
-// Computer the MD5 sum of a file
-//
-// file_name: The null terminated name of the file to be summed
-// md5_sum: A buffer large enough to receive the text MD5SUM
-//
-// Returns: zero on failure, non-zero on success.
-//
-int CalcFileMD5(char *file_name, char *md5_sum);
